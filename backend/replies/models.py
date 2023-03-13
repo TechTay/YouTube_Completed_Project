3c6d2ee31abb99a6f4e589f780066225ccd8a8ba
DeVante Parker <Devante.parker15@gmail.com>
2023-03-13 18:45:34 -0500

    Made migrations for comments and replies successfully.

--- backend/replies/models.py
+++ backend/replies/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from authentication.models import User
-
+from comments.models import Comment
 
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     text = models.CharField(max_length=100)
